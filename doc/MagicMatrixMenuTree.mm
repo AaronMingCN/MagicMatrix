c79c5e8b0ac623c6a6b57d1489ee6cff58bcb495
AaronMingCN <83288737+AaronMingCN@users.noreply.github.com>
2024-02-26 09:28:13 +0800

    update

--- doc/MagicMatrixMenuTree.mm
+++ doc/MagicMatrixMenuTree.mm
@@ -1,20 +1,20 @@
-<map version="freeplane 1.9.13">
+<map version="freeplane 1.11.5">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="MagicMatrix" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1708906909078">
+<node TEXT="MagicMatrix菜单树" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1708909400160" NUMBERED="false" FORMAT="markdownPatternFormat">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <font BOLD="true"/>
-<hook NAME="MapStyle" background="#f9f9f8" zoom="2.198">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="1.301">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/light_nord_template.mm"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
-<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
 <arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
 <edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
-<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
 <richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
@@ -33,7 +33,7 @@
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
 <icon BUILTIN="yes"/>
 <arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
@@ -41,7 +41,7 @@
 <edge COLOR="#bf616a"/>
 </stylenode>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
 <font SIZE="18"/>
 </stylenode>
@@ -79,20 +79,71 @@
 </stylenode>
 </map_styles>
 </hook>
-<node TEXT="基本显示" POSITION="right" ID="ID_1420311245" CREATED="1708906913766" MODIFIED="1708906928331">
-<node TEXT="0.时钟" ID="ID_537966244" CREATED="1708906929245" MODIFIED="1708907020186"/>
-<node TEXT="1.日期" ID="ID_1756253142" CREATED="1708906934777" MODIFIED="1708907024699"/>
-<node TEXT="2.环境温湿度" ID="ID_1525847969" CREATED="1708906937496" MODIFIED="1708907028258"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+<node TEXT="基本显示" POSITION="bottom_or_right" ID="ID_1420311245" CREATED="1708906913766" MODIFIED="1708909400160" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+<node TEXT="0.时钟" ID="ID_537966244" CREATED="1708906929245" MODIFIED="1708909400161" NUMBERED="false" FORMAT="markdownPatternFormat">
+<icon BUILTIN="button_ok"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
 </node>
-<node TEXT="填充" POSITION="right" ID="ID_1326429327" CREATED="1708907030650" MODIFIED="1708907039167">
-<node TEXT="0.行填充" ID="ID_925049962" CREATED="1708907041107" MODIFIED="1708907059299"/>
-<node TEXT="1.列填充" ID="ID_581850488" CREATED="1708907059835" MODIFIED="1708907065567"/>
-<node TEXT="2.对角线填充" ID="ID_1086317210" CREATED="1708907066039" MODIFIED="1708907075533"/>
-<node TEXT="3.蛇形填充" ID="ID_1789984015" CREATED="1708907075957" MODIFIED="1708907135592"/>
+<node TEXT="1.日期" ID="ID_1756253142" CREATED="1708906934777" MODIFIED="1708909400162" NUMBERED="false" FORMAT="markdownPatternFormat">
+<icon BUILTIN="button_ok"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
 </node>
-<node TEXT="图像显示" POSITION="right" ID="ID_1799667714" CREATED="1708907138472" MODIFIED="1708907144280">
-<node TEXT="dddd" ID="ID_1109368198" CREATED="1708907145208" MODIFIED="1708907260026"/>
-<node TEXT="" ID="ID_1504471942" CREATED="1708907170618" MODIFIED="1708907170618"/>
+<node TEXT="2.环境温湿度" ID="ID_1525847969" CREATED="1708906937496" MODIFIED="1708909400162" NUMBERED="false" FORMAT="markdownPatternFormat">
+<icon BUILTIN="button_ok"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+</node>
+</node>
+<node TEXT="填充" POSITION="bottom_or_right" ID="ID_1326429327" CREATED="1708907030650" MODIFIED="1708909400162" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+<node TEXT="0.行填充" ID="ID_925049962" CREATED="1708907041107" MODIFIED="1708909400163" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+</node>
+<node TEXT="1.列填充" ID="ID_581850488" CREATED="1708907059835" MODIFIED="1708909400163" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+</node>
+<node TEXT="2.对角线填充" ID="ID_1086317210" CREATED="1708907066039" MODIFIED="1708909400164" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+</node>
+<node TEXT="3.蛇形填充" ID="ID_1789984015" CREATED="1708907075957" MODIFIED="1708909400164" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+</node>
+</node>
+<node TEXT="图像显示" POSITION="bottom_or_right" ID="ID_1799667714" CREATED="1708907138472" MODIFIED="1708909400165" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+<node TEXT="0.静态图片循环显示" ID="ID_1109368198" CREATED="1708907145208" MODIFIED="1708909390749" NUMBERED="false" FORMAT="markdownPatternFormat"><richcontent TYPE="DETAILS" CONTENT-TYPE="plain/markdown">
+<text>+ 将图片放置在SD卡的bmp文件夹下</text></richcontent>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+</node>
+<node TEXT="1.动图显示" ID="ID_1504471942" CREATED="1708907170618" MODIFIED="1708909400165" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+</node>
+</node>
+<node TEXT="游戏" POSITION="bottom_or_right" ID="ID_1794420965" CREATED="1708908236466" MODIFIED="1708909400165" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+<node TEXT="0.记分牌" ID="ID_265575124" CREATED="1708908242100" MODIFIED="1708909400166" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+</node>
+<node TEXT="1.随机数抽取" ID="ID_1703852976" CREATED="1708908251296" MODIFIED="1708909400158" NUMBERED="false" FORMAT="markdownPatternFormat"><richcontent TYPE="DETAILS" CONTENT-TYPE="xml/markdown">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      + 通过按键数字选取1~n之间的随机数
+    </p>
+  </body>
+</html></richcontent>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+</node>
+<node TEXT="2.五子棋" ID="ID_833928992" CREATED="1708908260606" MODIFIED="1708909400166" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+</node>
+<node TEXT="3.贪食蛇" ID="ID_730534882" CREATED="1708908286255" MODIFIED="1708909400167" NUMBERED="false" FORMAT="markdownPatternFormat">
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown"/>
+</node>
 </node>
 </node>
 </map>
