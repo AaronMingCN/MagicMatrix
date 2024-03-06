65af55711314513be4c93d33b3214236f3f7bf8a
AaronMingCN <83288737+AaronMingCN@users.noreply.github.com>
2024-03-07 00:27:47 +0800

    update

--- src/MMFunc/MMF_CountDown.hpp
+++ src/MMFunc/MMF_CountDown.hpp
@@ -1,7 +1,7 @@
 /*
  * @File :  MMF_CountDown.hpp
  * @Time :  2024/03/06 19:44:55
- * @Auth :  
+ * @Auth :
  * @Vers :  1.0
  * @Desc :  倒计时功能模块
  */
@@ -19,7 +19,6 @@ public:
     MMF_CountDown(uint16_t fid)
         : MMFunc(fid)
     {
-        
     }
     uint16_t CurrRMinu = 0; // 剩余分钟
     uint16_t CurrRSec = 0; // 剩余秒钟
@@ -41,19 +40,48 @@ public:
         mmhardware.matrix.show();
     }
 
+    // 如果改变了则绘制
+    void DispRemainChange()
+    {
+        // 如果分数发生改变则开始绘图
+        if ((CurrRMinu != NextRMinu) || (CurrRSec != NextRSec)) {
+            CurrRMinu = NextRMinu;
+            CurrRSec = NextRSec;
+            this->DispRemain();
+        }
+    }    
+
     virtual MMFExecR_t Exec(InquireDelay* IDelay)
     {
         bool bp = true;
         this->DispRemain();
+        uint16_t irk = 0; // 定义红外线按键值
         do {
-            bp = !bp;
-            mmhardware.Beep(bp);
-            delay(100);
+            if (mmhardware.IRRCode(irk, true)) { // 读取红外线值
+                switch (irk) {
+                case IRK_UP:
+                    NextRMinu += 5;
+                    break;
+                case IRK_DOWN:
+                    NextRMinu -= 5;
+                    break;
+                case IRK_LEFT:
+                    ++NextRMinu;
+                    break;
+                case IRK_RIGHT:
+                    --NextRMinu;
+                    break;
+                case IRK_SET: // 按set键开始
+                    break;
+                default:
+                    break;
+                }
 
-        } while (IDelay->IDelay(10));
+                this->DispRemainChange(); // 如果剩余时间改变则显示
+            }
+        } while (IDelay->IDelay(100));
         mmhardware.Beep(false);
         return EXECR_OK;
-
     }
 };
 
